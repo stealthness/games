95fc79e12a3e26f299c30954ef045f84cd22ce9c
Stealthness <stevewestinlondon@yahoo.com>
2016-12-23 16:06:46 +0000

    Revert "1 file"
    This reverts commit 345ab9aa9915f49f302b593c3db3b37d51ec830b.

--- TestHelloWorldGame/src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Stephen West on 23/12/2016.
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world you won (test)");
-    }
-}
