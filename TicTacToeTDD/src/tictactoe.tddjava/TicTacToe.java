bcf65a66f3cb2f1860091961d53327fc45bd2483
Stealthness <stevewestinlondon@yahoo.com>
2018-11-28 17:50:27 +0000

    refactoring

--- TicTacToeTDD/src/tictactoe.tddjava/TicTacToe.java
+++ TicTacToeTDD/src/tictactoe.tddjava/TicTacToe.java
@@ -12,15 +12,23 @@ public class TicTacToe {
     };
 
     public void play(int row, int col) {
-        if (row < 1 || row > 3 ){
-            throw new RuntimeException("Y is outside the bounds of the board");
-        }else if (col < 1 || col > 3  ){
-            throw new RuntimeException("Y is outside the bounds of the board");
-        }
+        checkAxis(row);
+        checkAxis(col);
+        setBox(row,col);
+
+    }
+
+    private void setBox(int row, int col){
         if (board[row-1][col-1] != '\0'){
-            throw new RuntimeException("Y is outside the bounds of the board");
+            throw new RuntimeException("Box is occupied");
         }else{
             board[row-1][col-1] = 'X';
+        }
+    }
+
+    private void  checkAxis(int x){
+        if (x < 1 || x > 3 ){
+            throw new RuntimeException("X is outside the bounds of the board");
         }
     }
 
