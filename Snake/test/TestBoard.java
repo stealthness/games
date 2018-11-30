39cc78a341b16724ae99a1113f6af82c6e804a3f
Stealthness <stevewestinlondon@yahoo.com>
2018-11-30 05:54:38 +0000

    refactored

--- Snake/test/TestBoard.java
+++ Snake/test/TestBoard.java
@@ -8,20 +8,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class TestBoard {
 
-    SnakeGame sg;
+    private SnakeGame sg;
+
+    private final int[] SIZE_5x5 = {5,5};
+    private final int[] SIZE_3x5 = {3,5};
+    private final int[] SIZE_4x4 = {4,4};
 
 
     @BeforeEach
     public void setUp(){
-        sg = new SnakeGame(new int[] {5,5});
+        sg = new SnakeGame(SIZE_5x5);
     }
 
 
 
     @Test
     public void testEmptyBoardOFSize5x5isCreatedCorrectly(){
-        int[] boardSize = {5,5};
-        sg = new SnakeGame(boardSize);
+        sg = new SnakeGame(SIZE_5x5);
         String expBoardString = TestUtils.getTestCase("data01.txt","e5x5");
 
         assertEquals(expBoardString,sg.getBoardString());
@@ -31,39 +34,24 @@ public class TestBoard {
 
     @Test
     public void testEmptyBoardOFSize3x5isCreatedCorrectly(){
-        sg = new SnakeGame(new int[] {3,5});
-
-        String expBoardString = TestUtils.getTestCase("data01.txt","e3x5");
-
-        assertEquals(expBoardString,sg.getBoardString());
+        sg = new SnakeGame(SIZE_3x5);
+        assertEquals(TestUtils.getTestCase("data01.txt","e3x5"), sg.getBoardString());
 
 
     }
 
     @Test
     public void testEmptyBoardOFSize4x4isCreatedCorrectly(){
-        sg = new SnakeGame(new int[] {4,4});
-
-        String expBoardString = TestUtils.getTestCase("data01.txt","e4x4");
-
-        assertEquals(expBoardString,sg.getBoardString());
-
-
+        sg = new SnakeGame(SIZE_4x4);
+        assertEquals(TestUtils.getTestCase("data01.txt","e4x4"), sg.getBoardString());
     }
 
     @Test
     public void testThatAllGetPosDetailIsEmptyOnAnEmptyBoard(){
-
-        assertEquals(BLOCK.IS_EMPTY,sg.at(new int[]{0,0}));
-
-        for (int row = 0;row <5 ;row++){
-            for (int col = 0; col <5;col++){
+        for (int row = 0;row <SIZE_5x5[0] ;row++){
+            for (int col = 0; col <SIZE_5x5[1];col++){
                 assertEquals(BLOCK.IS_EMPTY,sg.at(new int[]{row,col}));
             }
         }
-
-
-
-
     }
 }
